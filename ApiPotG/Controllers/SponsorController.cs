300034438fbae9bed0bc93d7908200de17393763
Kristian Jeppe Schou Guld <kjsguld@gmail.com>
2018-05-11 22:02:25 +0200

    Sponsor controller and single item API get

---
+++ ApiPotG/Controllers/SponsorController.cs
@@ -0,0 +1,41 @@
+﻿using ApiPotG.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Umbraco.Core;
+using Umbraco.Web.WebApi;
+
+namespace ApiPotG.Controllers
+{
+    public class SponsorController : UmbracoApiController
+    {
+        [HttpGet]
+        public Sponsor GetSponsor(int sponsorId)
+        {
+            var cs = Services.ContentService;
+            var sponsor = cs.GetById(sponsorId);
+
+            Sponsor res = new Sponsor
+            {
+                id = sponsor.Id,
+                sponsorName = sponsor.Properties["sponsorName"].Value.ToString()
+            };
+
+            try
+            {
+                string guid = sponsor.Properties["logo"].Value.ToString();
+                var udi = Udi.Parse(guid);
+                var media = Umbraco.GetIdForUdi(udi);
+                var content = Umbraco.Media(media);
+                var imgPath = content.Url;
+                res.logo = imgPath;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ ApiPotG/Models/Sponsor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiPotG.Models
+{
+    public class Sponsor
+    {
+        public int id { get; set; }
+        public string sponsorName { get; set; }
+        public string logo { get; set; }
+    }
+}
