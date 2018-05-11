ac239e820114e18a2bf1bd89b98dc6bc544e0a13
Kristian Jeppe Schou Guld <kjsguld@gmail.com>
2018-05-12 01:08:00 +0200

    Votes setup without validation

---
+++ ApiPotG/Controllers/VoteController.cs
@@ -0,0 +1,80 @@
+﻿using ApiPotG.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using Umbraco.Web.WebApi;
+
+namespace ApiPotG.Controllers
+{
+  
+    public class VoteController : UmbracoApiController
+    {
+        private readonly IContentService cs;
+
+        public VoteController()
+        {
+            cs = Services.ContentService;
+        }
+
+        [HttpPost]
+        public string CommitVote([FromBody] Vote data)
+        {
+            
+            try
+            {
+                //validate vote
+                //bool validates = ValidateVote(ticketID, projectID);
+                //if (validates)
+                //{
+                string message = "api entry ticket for match: " + data.matchId.ToString() + ", by: " + data.IMEI.ToString();
+                //create vote object
+                var vote = cs.CreateContent(message, data.voteBatchId, "vote");
+
+                //set vote properties
+                vote.Properties["IMEI"].Value = data.IMEI;
+                vote.Properties["matchId"].Value = data.matchId;
+                vote.Properties["playerId"].Value = data.playerId;
+
+                cs.Publish(vote);
+                cs.Save(vote);
+                return "success";
+                //}
+                //if (!validates)
+                //{
+                //    return "The vote is invalid";
+                //}
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return "failed to vote";
+        }
+
+        [HttpGet]
+        public List<Vote> GetVotes(int voteBatchId)
+        {
+            List<Vote> res = new List<Vote>();
+            var votes = cs.GetDescendants(voteBatchId);
+
+            foreach (var vote in votes)
+            {
+                var v = new Vote
+                {
+                    id = vote.Id,
+                    IMEI = int.Parse(vote.Properties["IMEI"].Value.ToString()),
+                    matchId = int.Parse(vote.Properties["matchId"].Value.ToString()),
+                    playerId = int.Parse(vote.Properties["playerId"].Value.ToString()),
+                    dateTime = vote.CreateDate
+                };
+                res.Add(v);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ ApiPotG/Models/Vote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiPotG.Models
+{
+    public class Vote
+    {
+        public int id { get; set; }
+        public int IMEI { get; set; }
+        public int matchId { get; set; }
+        public int playerId { get; set; }
+        public DateTime dateTime { get; set; }
+        public int voteBatchId { get; set; }
+    }
+}
